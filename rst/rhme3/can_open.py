eb67c4182582018864dba9b300914b2ac0da5cca
keesj <kees.jongenburger@gmail.com>
2018-05-29 08:38:30 +0200

    Small tweak but can_open is Working now

--- rst/rhme3/can_open.py
+++ rst/rhme3/can_open.py
@@ -13,11 +13,14 @@ def send_frame(id,data):
 	dev.send(frame)
 
 
-d= list(bytearray("lock\0\0\0\0"))
+d= list(bytearray("unlock\0\0"))
 
 id = 0x332
 
 while True:
-	time.sleep(1)
+	time.sleep(.1)
 	send_frame(id,d)
-  
+	while dev.available():
+		f = dev.recv()
+		if f is not None:
+			print(f)
